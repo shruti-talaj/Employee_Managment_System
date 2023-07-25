b8f9ec40e4c73de86769627a4045e1fa336cb789
shrutitalaj1709 <119961691+shruti-talaj@users.noreply.github.com>
2023-07-25 16:27:07 +0530

    Update Query.java

--- src/Query.java
+++ src/Query.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class Query {
+public class Query {
+    public static String select ="Select * from Emp ";
+    public static String Insert = "insert into Emp values('"+Empinfo.getId()+"','"+ Empinfo.getName()+"','"+Empinfo.getAdd()+"','"+Empinfo.getPhone()+"','"+Empinfo.getEmail()+"','"+Empinfo.getSalary()+"','"+Empinfo.getDept()+"')";
 }
